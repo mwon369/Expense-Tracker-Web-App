4e9aab3d83ee7fdf29987bf1edea4e648c1161c2
Mitchell <mitchellwong833@gmail.com>
2023-04-02 15:50:34 +1200

    Added controller logic for API endpoints

---
+++ backend/controllers/transactionController.ts
@@ -0,0 +1,72 @@
+import { Request, Response } from 'express';
+
+const Transaction = require('../models/transactionModel');
+const mongoose = require('mongoose');
+
+const getAllTransactions = async (req: Request, res: Response) => {
+    const transactions = await Transaction.find({}).sort({ date: -1 });
+    res.status(200).json(transactions);
+}
+
+const getSingleTransactionByID = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'Invalid transaction ID'});
+    }
+
+    const transaction = await Transaction.findById(id);
+    if (!transaction) {
+        return res.status(404).json({ error: 'Transaction does not exist'});
+    }
+
+    res.status(200).json(transaction);
+}
+
+const createNewTransaction = async (req: Request, res: Response) => {
+    const { value, date, transactionType, description } = req.body;
+
+    try {
+        const transaction = await Transaction.create({ value, date, transactionType, description });
+        res.status(200).json(transaction);
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            res.status(400).json({ error: error.message });
+        }
+    }
+}
+
+const updateSingleTransactionByID = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'Invalid transaction ID'});
+    }
+
+    const transaction = await Transaction.findByIdAndUpdate(id, { ...req.body });
+    if (!transaction) {
+        return res.status(404).json({ error: 'Transaction does not exist'});
+    }
+
+    res.status(200).json(transaction);
+}
+
+const deleteSingleTransactionByID = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'Invalid transaction ID'});
+    }
+
+    const transaction = await Transaction.findByIdAndDelete(id);
+    if (!transaction) {
+        return res.status(404).json({ error: 'Transaction does not exist'});
+    }
+
+    res.status(200).json(transaction);
+}
+
+module.exports = {
+    getAllTransactions,
+    getSingleTransactionByID,
+    createNewTransaction,
+    updateSingleTransactionByID,
+    deleteSingleTransactionByID
+}
--- backend/routes/transactions.ts
+++ backend/routes/transactions.ts
@@ -1,37 +1,22 @@
-import { Request, Response } from 'express';
-
 const express = require('express');
-const Transaction = require('../models/transactionModel');
+const {
+    getAllTransactions,
+    getSingleTransactionByID,
+    createNewTransaction,
+    updateSingleTransactionByID,
+    deleteSingleTransactionByID
+} = require('../controllers/transactionController');
+
 const router = express.Router();
 
-router.get('/', (req: Request, res: Response) => {
-    return res.json({msg: 'GET ALL'});
-})
+router.get('/', getAllTransactions);
 
-router.get('/:id', (req: Request, res: Response) => {
-    return res.json({msg: 'GET ID'});
-})
+router.get('/:id', getSingleTransactionByID);
 
-router.post('/', async (req: Request, res : Response) => {
-    const { value, date, transactionType, description } = req.body;
+router.post('/', createNewTransaction);
 
-    try {
-        const transaction = await Transaction.create({ value, date, transactionType, description });
-        res.status(200).json(transaction);
-    } catch (error: unknown) {
-        if (error instanceof Error) {
-            res.status(400).json({ error: error.message });
-        }
-    }
-})
+router.patch('/:id', updateSingleTransactionByID);
 
-router.patch('/:id', (req: Request, res: Response) => {
-    return res.json({msg: 'PATCH'});
-})
-
-router.delete('/:id', (req: Request, res: Response) => {
-    return res.json({msg: 'DELETE'});
-})
-
+router.delete('/:id', deleteSingleTransactionByID);
 
 module.exports = router;
