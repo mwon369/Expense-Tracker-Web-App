476e6b59ba4dad53388ecfd0407b6518676d43c4
Mitchell <mitchellwong833@gmail.com>
2023-04-12 17:05:22 +1200

    Implemented login in the backend with JWT's

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -1,18 +1,43 @@
 import { Request, Response } from 'express';
+import { ObjectId } from 'mongoose';
 
-const bcrypt = require('bcrypt');
+require('dotenv').config();
+import bcrypt = require('bcrypt');
+import jwt = require('jsonwebtoken');
 const validator = require('validator');
 const User = require('../models/userModel');
 
+const createToken = (_id: ObjectId) => {
+    const secret = process.env.SECRET;
+    return secret ? jwt.sign({ _id }, secret, { expiresIn: '3d' }) : null;
+}
+
 const loginUser = async (req: Request, res: Response) => {
-    res.json({msg: 'logged in'});
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({ error: 'All fields must be filled' });
+    }
+
+    const user = await User.findOne({ username });
+    if (!user) {
+        return res.status(400).json({ error: 'Incorrect username' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+        const token = createToken(user._id);
+        return res.status(200).json({ username, token });
+    }
+
+    return res.status(400).json({ error: 'Incorrect password' })
 }
 
 const signupUser = async (req: Request, res: Response) => {
     const { username, password } = req.body;
 
     if (!username || !password) {
-        return res.status(400).json({ error: 'All fields must be filled'});
+        return res.status(400).json({ error: 'All fields must be filled' });
     }
 
     if (!validator.isStrongPassword(password)) {
@@ -23,16 +48,17 @@ const signupUser = async (req: Request, res: Response) => {
     }
 
     const exists = await User.findOne({ username });
-
     if (exists) {
-        return res.status(400).json({ error: `Username '${username}' is already in use.`});
+        return res.status(400).json({ error: `Username '${username}' is already in use` });
     }
 
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
 
     const user = await User.create({ username, password: hash });
-    res.status(200).json({ username, user });
+    const token = createToken(user._id);
+    
+    res.status(200).json({ username, token });
 }
 
 module.exports = { loginUser, signupUser };
